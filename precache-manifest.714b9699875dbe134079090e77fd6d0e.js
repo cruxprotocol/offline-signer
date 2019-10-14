5693493c922f20dac68d05d4492c29353790c6f6
Amit Singh <amit@coinswitch.co>
2019-10-14 19:40:28 +0530

    Updates

---
+++ precache-manifest.714b9699875dbe134079090e77fd6d0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2842fde0a9d98e27471820b6154adfc2",
+    "url": "../../offline-signer/index.html"
+  },
+  {
+    "revision": "e1ddcf9da89714e02149",
+    "url": "../../offline-signer/static/css/main.c8f9598f.chunk.css"
+  },
+  {
+    "revision": "adefa5b0447024bed4a9",
+    "url": "../../offline-signer/static/js/2.c1d4a07b.chunk.js"
+  },
+  {
+    "revision": "e1ddcf9da89714e02149",
+    "url": "../../offline-signer/static/js/main.d70ccb69.chunk.js"
+  },
+  {
+    "revision": "f2d0f39756e803789b25",
+    "url": "../../offline-signer/static/js/runtime-main.40058938.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "../../precache-manifest.a005530a073ad6e7dac1edd0f6573b6f.js"
+  "../../offline-signer/precache-manifest.714b9699875dbe134079090e77fd6d0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../../index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../../offline-signer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

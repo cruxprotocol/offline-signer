9b8e1a6a518e9f72aad750b961d768761d8bdec6
Amit Singh <amit@coinswitch.co>
2019-10-15 19:48:09 +0530

    Updates

---
+++ precache-manifest.fcf043eb379b227240b8809e5897eaf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cc3ec4f674e8aa9a3098a97f9997d7",
+    "url": "../../offline-signer/index.html"
+  },
+  {
+    "revision": "1ce3ac12f47fe044ee6e",
+    "url": "../../offline-signer/static/css/main.428feb1f.chunk.css"
+  },
+  {
+    "revision": "1a7a4711ebb6f107eedd",
+    "url": "../../offline-signer/static/js/2.c3340035.chunk.js"
+  },
+  {
+    "revision": "1ce3ac12f47fe044ee6e",
+    "url": "../../offline-signer/static/js/main.847a6659.chunk.js"
+  },
+  {
+    "revision": "f2d0f39756e803789b25",
+    "url": "../../offline-signer/static/js/runtime-main.40058938.js"
+  }
+]);

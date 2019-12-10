a1699b883102e6991db0ded83fc319bfc918f6e5
Amit Singh <amit@coinswitch.co>
2019-12-10 12:19:48 +0530

    Updates

---
+++ precache-manifest.d225fc2556ea53879323b1cf8b93da3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4374bb0795472cab157df7ea045ac9",
+    "url": "../../offline-signer/index.html"
+  },
+  {
+    "revision": "6b5113a44640440a52ec",
+    "url": "../../offline-signer/static/css/main.f7ff3466.chunk.css"
+  },
+  {
+    "revision": "27fd44f710dcb434f238",
+    "url": "../../offline-signer/static/js/2.0b6b402f.chunk.js"
+  },
+  {
+    "revision": "8ae2654c4ea96088282ca18cd9c1efa6",
+    "url": "../../offline-signer/static/js/2.0b6b402f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b5113a44640440a52ec",
+    "url": "../../offline-signer/static/js/main.960badc4.chunk.js"
+  },
+  {
+    "revision": "0ef75d81ea34a99a5fe3",
+    "url": "../../offline-signer/static/js/runtime-main.b60d6641.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "../../offline-signer/precache-manifest.327d3ef165ed83d8e69c67a5830622e6.js"
+  "../../offline-signer/precache-manifest.d225fc2556ea53879323b1cf8b93da3c.js"
 );
 
 self.addEventListener('message', (event) => {
